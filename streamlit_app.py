75deb76d77ff66fec45a16fb32cbf2cc5e4ce167
mariogalvis <mariogalvisguerrero@gmail.com>
2023-07-26 16:43:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import requests
 import pandas
 import snowflake.connector
-from urllib.error import URLError
+#from urllib.error import URLError
 
 streamlit.title("My parents new healthy diner")
 
